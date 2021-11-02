112207606fc363314830a8ba4c434534e7d85315
audwl <93104498+audwl@users.noreply.github.com>
2021-11-02 14:26:31 +0900

    Update 03_ec2.tf

--- 01_module/03_ec2.tf
+++ 01_module/03_ec2.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "mjkim_websg" {
         description = var.prot_http
         from_port = var.http_port
         to_port = var.http_port
-        protocol = var.prot_http
+        protocol = var.prot_tcp
         cidr_blocks = [var.cidr]
         ipv6_cidr_blocks = [var.cidr_v6]
         security_groups = null
@@ -87,10 +87,10 @@ data "aws_ami" "amzn" {
 
 resource "aws_instance" "mjkim_web" {
   ami                    = "ami-0e4a9ad2eb120e054"                             #data.aws_ami.amzn.id
-  instance_type          = "t2.micro"
+  instance_type          = var.instance
   key_name               = var.key
   vpc_security_group_ids = [aws_security_group.mjkim_websg.id]
-  availability_zone      = "${var.region}"
+  availability_zone      = "${var.region}${var.avazone[0]}"
   private_ip             = var.private_ip
   subnet_id              = aws_subnet.mjkim_pub[0].id
   user_data              = file("./install_seoul.sh") 
