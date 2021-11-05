c3eab6e1ec036f41e0cb20ebb42d90167634a9b1
audwl <93104498+audwl@users.noreply.github.com>
2021-11-05 09:24:24 +0900

    Update 06_rds.tf

--- 01_module/06_rds.tf
+++ 01_module/06_rds.tf
@@ -1,27 +1,34 @@
 resource "aws_db_instance" "mjkim_mydb" {
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  engine                  = var.prot_mysql
-  engine_version          = "8.0"
-  instance_class          = "db.t2.micro"
+  allocated_storage       = var.storage_size
+  storage_type            = var.storage_type
+  engine                  = var.sql_engine
+  engine_version          = var.mysql_version
+  instance_class          = var.instance_db
   name                    = var.name_db
   identifier              = var.name_db
-  username                = "admin"
-  password                = "It12345!"
+  username                = var.username
+  password                = var.password
   parameter_group_name    = "default.mysql8.0"
   availability_zone       = "${var.region}${var.avazone[0]}"
   db_subnet_group_name    = aws_db_subnet_group.mjkim_dbsn.id
   vpc_security_group_ids  = [aws_security_group.mjkim_websg.id]
   skip_final_snapshot     = true
   tags = {
-      Name = "${var.name}-mydb"
+      Name = "${var.name}-${var.name_db}"
   }
 }
 
 resource "aws_db_subnet_group" "mjkim_dbsn" {
-  name  =   "mjkim-dbsb-group"
+  name  =   "${var.name}-dbsb-group"
   subnet_ids = [aws_subnet.mjkim_pridb[0].id,aws_subnet.mjkim_pridb[1].id]
   tags = {
-      Name = "mjkim-dbsb-group"
+      Name = "${var.name}-dbsb-group"
   }
+}
+
+data "aws_db_instance" "mydb" {
+  db_instance_identifier = "mydb"
+  depends_on = [
+    aws_db_instance.mjkim_mydb
+  ]
 }
